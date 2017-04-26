05bc48a7b073eefdfffa90a95adae3626f068b81
Lanxor <Thomas.olivier3@yahoo.fr>
2017-04-26 18:10:40 +0200

    Initialisation header.

--- gestion/player.h
+++ gestion/player.h
@@ -1,14 +1,32 @@
-/* 
- * File:   player.h
- * Author: xiodi
- *
- * Created on 26 avril 2017, 17:26
+/**
+ * @file player.h
+ * @author PATAT Gwendal
+ * @date 26 avril 2017
+ * @brief Interface de la gestion des joueurs.
  */
 
 #ifndef __PLAYER_H__
 #define __PLAYER_H__
 
+/**
+ * @brief Structure du joueur
+ */
 
+/**
+ * @brief Créer une joueur vide.
+ */
+
+/**
+ * @brief Initialiser un joueur.
+ */
+
+/**
+ * @brief Supprimer un joueur.
+ */
+
+/**
+ * @brief Libérer un joueur.
+ */
 
 #endif /* PLAYER_H */
 
