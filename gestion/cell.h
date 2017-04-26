69109954e49427dc7cd5d48a8c13dbb820052d69
Lanxor <Thomas.olivier3@yahoo.fr>
2017-04-26 18:02:06 +0200

    Initialisation header

--- gestion/cell.h
+++ gestion/cell.h
@@ -1,13 +1,34 @@
-/* 
- * File:   cell.h
- * Author: xiodi
- *
- * Created on 26 avril 2017, 17:29
+/**
+ * @file cell.h
+ * @author PATAT Gwendal
+ * @date 26 avril 2017
+ * @brief Interface de la gestion de case
  */
 
 #ifndef __CELL_H__
 #define __CELL_H__
 
+#include <stdlib.h>
+#include <stdio.h>
 
-#endif /* CELL_H */
+/**
+ * @brief Structure d'une case d'un tablier.
+ */
 
+/**
+ * @brief Créer une case.
+ */
+
+/**
+ * @brief Modifier une case.
+ */
+
+/**
+ * @brief Supprimer une case.
+ */
+
+/**
+ * @brief Libérer une case.
+ */
+
+#endif
--- gestion/deck.h
+++ gestion/deck.h
@@ -1,9 +1,55 @@
+/**
+ * @file deck.h
+ * @author PATAT Gwendal
+ * @date 26 avril 2017
+ * @brief Interface de la gestion du tablier
+ */
+
 #ifndef __DECK_H__
 #define __DECK_H__
 
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * @brief Structure du tablier.
+ */
 typedef struct s_deck* deck;
+
+/**
+ * @brief Crée un tablier vide.
+ */
+
+/**
+ * @brief Modifie une case du tablier.
+ */
+
+/**
+ * @brief Supprimer un tablier.
+ */
+
+/**
+ * @brief Libérer un tablier.
+ */
+
+/**
+ * @brief Jouer un coup.
+ */
+
+/**
+ * @brief Vérifier la légalité d'un coup.
+ */
+
+/**
+ * @brief Déterminer un vainqueur.
+ */
+
+/**
+ * @brief Sauvegarder une partie en cours.
+ */
+
+/**
+ * @brief Restaurer une partie en cours.
+ */
 
 #endif
