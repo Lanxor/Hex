b70593482b0024ac880e1ad7eabd83634d3d189c
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-02 18:29:41 +0200

    Construction des fonctions de base de gestion de coordonnées, et modification de la structure des noms x -> abscisse et y -> ordonnee

--- gestion/coordinates.c
+++ gestion/coordinates.c
@@ -9,6 +9,40 @@
 
 typedef struct s_coordinates
 {
-  unsigned int	x;
-  unsigned int	y;
+  unsigned int	abscisse;
+  unsigned int	ordonnee;
 } t_coordinates;
+
+Coordinates coordinates_create(unsigned int abscisse, 
+                               unsigned int ordonnee)
+{
+    Coordinates coordinates;
+    
+    coordinates = malloc(sizeof(t_coordinates));
+    assert( coordinates != NULL );
+    
+    coordinates->abscisse = abscisse;
+    coordinates->ordonnee = ordonnee;
+    
+    return coordinates;
+}
+
+Coordinates coordinates_modify(Coordinates coordinates, 
+                               unsigned int abscisse,
+                               unsigned int ordonnee)
+{
+    coordinates->abscisse = abscisse;
+    coordinates->ordonnee = ordonnee;
+    
+    return coordinates;
+}
+
+void coordinaes_delete(Coordinates coordinates)
+{
+    coordinates_free(coordinates);
+}
+
+void coordinates_free(Coordinates coordinates)
+{
+    free(coordinates);
+}
