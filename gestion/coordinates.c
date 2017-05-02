4c84d4e7fa1a025c80656329273955ee7d6b301b
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-02 10:24:44 +0200

    Initialisation du code Coordinates

---
+++ gestion/coordinates.c
@@ -0,0 +1,14 @@
+/**
+ * @file coordinates.c
+ * @author PATAT Gwendal
+ * @date 26 avril 2017
+ * @brief Code gestion des coordonées.
+ */
+
+#include "coordinates.h"
+
+typedef struct s_coordinates
+{
+  unsigned int	x;
+  unsigned int	y;
+} t_coordinates;
