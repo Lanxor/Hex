28dcbec457214cd743729e675e1668cd115a237b
Eléana <Elana@Grald>
2017-05-02 18:27:25 +0200

    maj menu

--- interface/src/pkginterface/Hex.java
+++ interface/src/pkginterface/Hex.java
@@ -8,6 +8,7 @@ package pkginterface;
 
 public class Hex {
     
+    private static Menu menu;
     private static Deck tablier;
     private int nbJoueurs = 2;
     private static Player Joueur1;
@@ -15,15 +16,21 @@ public class Hex {
     
     public static void main(String[] args)
     {      
-        tablier = new Deck();
-        tablier.init();
-        System.out.println("Joueur 1");
-        Joueur1 = new Player(tablier);
-        Joueur1.init();
-        System.out.println("Joueur 2");
-        Joueur2 = new Player(tablier);
-        Joueur2.init();
-        tablier.displayBoard();
+        menu = new Menu();
+        menu.displayHome();
+        int resp = menu.choiceHome();
+        if (resp == 1){
+            tablier = new Deck();
+            tablier.init();
+            System.out.println("Joueur 1");
+            Joueur1 = new Player(tablier);
+            Joueur1.init();
+            System.out.println("Joueur 2");
+            Joueur2 = new Player(tablier);
+            Joueur2.init();
+            tablier.displayBoard();
+        }
+        
         Joueur1.placeStone();
         tablier.displayBoard();
         Joueur2.placeStone();
