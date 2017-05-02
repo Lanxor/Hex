653cffd64c06b83d9086ee8e9e950a6b829b9570
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-02 18:24:41 +0200

    Modification des commentaires des prototypes

--- gestion/coordinates.h
+++ gestion/coordinates.h
@@ -10,6 +10,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 /**
  * @brief Structure des coordonées
@@ -18,29 +19,39 @@ typedef struct s_coordinates *Coordinates;
 
 /**
  * @brief Crée de nouvelles coordonées.
- * @param int x
- * @param int y
+ * @fn Coordinates coordinates_create(unsigned int abscisse, 
+ *                                    unsigned int ordonnee)
+ * @param abscisse
+ * @param ordonnee
  * @return Retourne de nouvelles coordonées initialisé.
  */
-Coordinates coordinates_create(int x, int y);
+Coordinates coordinates_create(unsigned int abscisse,
+                               unsigned int ordonnee);
 
 /**
  * @brief Modifier les coordonées.
+ * @fn Coordinates coordinates_modify(Coordinates coordinates, 
+ *                                    unsigned int abscisse, 
+ *                                    unsigned int ordonnee)
  * @param Coordinates coordinates
- * @param int x
- * @param int y
+ * @param abscisse
+ * @param ordonnee
  * @return Retourne les coordonées modifier.
  */
-Coordinates coordinates_modify(Coordinates coordinates, int x, int y);
+Coordinates coordinates_modify(Coordinates coordinates, 
+                               unsigned int abscisse,
+                               unsigned int ordonnee);
 
 /**
  * @brief Supprimer les coordonées
+ * @fn void coordinates_delete(Coordinates coordinates)
  * @param Coordinates coordinates
  */
 void coordinates_delete(Coordinates coordinates);
 
 /**
  * @brief Libérer les coordonées
+ * @fn void coordinates_free(Coordinates coordinates)
  * @param Coordinates coordinates
  */
 void coordinates_free(Coordinates coordinates);
