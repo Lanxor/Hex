c9a9a0dc3092014a18d77127ec862eb2b422f0c4
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-02 18:16:11 +0200

    Construction des fonctions de bases de gestions de la cellules

--- gestion/cell.c
+++ gestion/cell.c
@@ -9,7 +9,48 @@
 
 typedef struct s_cell
 {
-  t_coordinates	*coordinates;
+  Coordinates coordinates;
   struct s_cell **adjacentCells;
   unsigned int	value; // replace with pointer to player
 } t_cell;
+
+Cell cell_create_void(void)
+{
+    return NULL;
+}
+
+Cell cell_create(Coordinates coordinates)
+{
+    Cell cell = malloc(sizeof(t_cell));
+    assert( cell != NULL );
+    
+    cell->coordinates = coordinates;
+    cell->value = 0;
+    
+    return cell;
+}
+
+void cell_modify_cell(Cell cell_old, Cell cell_new)
+{
+    cell_old = cell_new;
+}
+
+void cell_modify_coordinates(Cell cell, Coordinates coordinates)
+{
+    cell->coordinates = coordinates;
+}
+
+void cell_modify_value(Cell cell, unsigned int value)
+{
+    cell->value = value;
+}
+
+void cell_delete(Cell cell)
+{
+    cell_free(cell);
+}
+
+void cell_free(Cell cell)
+{
+    free(cell);
+}
