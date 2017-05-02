0cc49cb6c7b080d46a611c78518174e2b0b769cb
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-02 10:28:02 +0200

    Initialisation code de la cellule

--- gestion/cell.c
+++ gestion/cell.c
@@ -4,3 +4,12 @@
  * @date 26 avril 2017
  * @brief Code gestion de cases.
  */
+
+#include "cell.h"
+
+typedef struct s_cell
+{
+  t_coordinates	*coordinates;
+  struct s_cell **adjacentCells;
+  unsigned int	value; // replace with pointer to player
+} t_cell;
