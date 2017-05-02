6b8c65f90c0e92802e3fd5af8bf318193dfa3bcb
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-02 10:13:14 +0200

    Définitions des prototypes

--- gestion/cell.h
+++ gestion/cell.h
@@ -14,21 +14,39 @@
 /**
  * @brief Structure d'une case d'un tablier.
  */
+typedef struct s_cell *Cell;
 
 /**
- * @brief Créer une case.
+ * @brief Créer une case vide.
+ * @param void
  */
+Cell cell_create(void);
+
+/**
+ * @brief Crée une nouvelle case avec des coordonnées.
+ * @param int x
+ * @param int y
+ * @return Une case avec des coordonées.
+ */
+Cell cell_create(int x, int y);
 
 /**
  * @brief Modifier une case.
+ * @param Cell cell
+ * @return Nouvelle cellules modifié.
  */
+Cell cell_modify(Cell cell);
 
 /**
  * @brief Supprimer une case.
+ * @param Cell cell
  */
+void cell_delete(Cell cell);
 
 /**
  * @brief Libérer une case.
+ * @param Cell cell
  */
+void cell_free(Cell cell);
 
 #endif
