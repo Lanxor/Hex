23b923307f0ec2c279115e45e7d1229d6dfda9d7
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-02 19:00:20 +0200

    Construction des première fonctions essentielles de la gestion du tablier.

--- gestion/deck.c
+++ gestion/deck.c
@@ -10,5 +10,39 @@
 typedef struct s_deck
 {
   Cell	**cells;
-  unsigned int	numberOfCells;
+  unsigned int	size;
 } t_deck;
+
+Deck deck_create(unsigned int size)
+{
+    Deck deck;
+    
+    deck = malloc(sizeof(t_deck));
+    assert( deck != NULL );
+    deck->size = size;
+    deck = deck_initialize_cells(Deck deck);
+
+    return deck;
+}
+
+Deck deck_initialize_cells(Deck deck)
+{
+    //TODO Il faut crée les size*size cellules
+}
+
+Deck deck_modify_size(Deck deck, unsigned int size)
+{
+    deck->size = size;
+    return deck;
+}
+
+void deck_delete(Deck deck)
+{
+    //TODO Il faut supprimer les cellules avec cell_delete()
+    deck_free(deck);
+}
+
+void deck_delete(Deck deck)
+{
+    free(deck);
+}
