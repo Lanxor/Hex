592c3bf2fc4fbec44e5e59aa3a05f292e846bda9
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-02 10:34:26 +0200

    Initialisation prototype du tablier

--- gestion/deck.h
+++ gestion/deck.h
@@ -11,14 +11,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "cell.h"
+
 /**
  * @brief Structure du tablier.
  */
-typedef struct s_deck* deck;
+typedef struct s_deck* Deck;
 
 /**
  * @brief Crée un tablier vide.
  */
+Deck deck_create();
 
 /**
  * @brief Modifie une case du tablier.
@@ -26,11 +29,15 @@ typedef struct s_deck* deck;
 
 /**
  * @brief Supprimer un tablier.
+ * @param Deck deck
  */
+void deck_delete(Deck deck);
 
 /**
  * @brief Libérer un tablier.
+ * @param Deck deck
  */
+void deck_free(Deck deck);
 
 /**
  * @brief Jouer un coup.
@@ -46,10 +53,22 @@ typedef struct s_deck* deck;
 
 /**
  * @brief Sauvegarder une partie en cours.
+ * @param Deck deck
+ * @param char *name_file
+ * @return Retourne si la partie à bien été sauvegardé.
+ * @returns 1 : Partie bien sauvegardé
+ * @returns 0 : Erreur
  */
+int deck_save_file(Deck deck, char *name_file);
 
 /**
  * @brief Restaurer une partie en cours.
+ * @param Deck deck
+ * @param char *name_file
+ * @return Retourne si la partie à bien été chargé.
+ * @returns 1 : Partie bien chargé
+ * @returns 0 : Erreur
  */
+int deck_restore_file(Deck deck, char *name_file);
 
 #endif
