18798bfceac0e43b9b306af29004ed142af8cf76
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-03 18:36:38 +0200

    Ajout du test print sur edge

--- gestion/tests/test_edge.c
+++ gestion/tests/test_edge.c
@@ -31,10 +31,25 @@ void test2_delete()
     printf("\n");
 }
 
+void test3_print()
+{
+    printf("\ttest3_print()\n");
+    Edge edge;
+    Vertice vertice1, vertice2;
+    
+    vertice1 = vertice_create(TRANSPARENT, 1, 2);
+    vertice2 = vertice_create(TRANSPARENT, 1, 3);
+    
+    edge = edge_create(vertice1, vertice2);
+    edge_print(edge);
+    printf("\n");
+}
+
 int main() {
     printf("\nTest Simple - TEST EDGE\n");
     test1_create();
     test2_delete();
+    test3_print();
 
     printf("\n");
     return (EXIT_SUCCESS);
