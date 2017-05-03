58ad56c4d01f83f0eed625ab6ad8c5bb07ddb9ec
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-03 18:14:08 +0200

    Maj : ajout des arêtes dans la structure

--- gestion/deck.c
+++ gestion/deck.c
@@ -14,8 +14,10 @@ typedef struct s_deck
 
 Deck deck_create(unsigned int size)
 {
+    int abscisse, ordonnee, number_edges, counter_edges;
     Deck deck;
-    Vertice new_vertice;
+    Vertice new_vertice, vertice_current, vertice_target;
+    Edge new_edge;
     
     deck = malloc(sizeof(t_deck));
     assert( deck != NULL );
@@ -27,15 +29,94 @@ Deck deck_create(unsigned int size)
     deck->black_2 = vertice_create(TRANSPARENT, -1, -1);
     
     deck->set_vertices = malloc(size*size*sizeof(Vertice));
+    // (4*size)+((3*size)*(size-2))+(size*2+1) est le nombre d'arête (sans compter les doublons)
+    number_edges = (4*size)+((3*size)*(size-2))+(size*2+1);
+    deck->set_edges = malloc( (number_edges)*sizeof(Edge) );
     
-    for(int abscisse = 0; abscisse < size; abscisse++)
+    for(abscisse = 0; abscisse < size; abscisse++)
     {
-        for(int ordonnee = 0; ordonnee < size; ordonnee++)
+        for(ordonnee = 0; ordonnee < size; ordonnee++)
         {
             new_vertice = vertice_create(TRANSPARENT, abscisse, ordonnee);
             deck->set_vertices[(deck->size*abscisse)+ordonnee] = new_vertice;
+            if ( abscisse == 0 )
+            {
+                new_edge = edge_create(deck->black_1, new_vertice);
+            }
+            else if ( abscisse == deck->size )
+            {
+                new_edge = edge_create(deck->black_2, new_vertice);
+            }
+            if ( ordonnee == 0 )
+            {
+                new_edge = edge_create(deck->white_1, new_vertice);
+            }
+            else if ( ordonnee == deck->size )
+            {
+                new_edge = edge_create(deck->white_2, new_vertice);
+            }
         }
     }
+    
+    counter_edges = 0;
+    
+    for(abscisse = 0; abscisse < size; abscisse++)
+    {
+        for(ordonnee = 0; ordonnee < size; ordonnee++)
+        {
+            vertice_current;
+            vertice_target;
+            // b1 jamais
+            // b2 jamais
+                // sauf si abscisse == 0 -> on le met à black
+            if ( abscisse == 0 )
+            {
+                vertice_target = deck->black_1;
+                new_edge = edge_create(vertice_current, vertice_target);
+                deck->set_edges[counter_edges++] = new_edge;
+            }
+            
+            // b3 tout le temps 
+                // si ordonnee+1 == deck->size -> on le met à white
+            if ( ordonnee+1 == deck->size )
+            {
+                vertice_target = deck->white_2;
+            }
+            else
+            {
+                vertice_target = vertice_get_coordinates(deck, abscisse, ordonnee+1);
+            }
+            new_edge = edge_create(vertice_current, vertice_target);
+            deck->set_edges[counter_edges++] = new_edge;
+            
+            // b4 tout le temps
+                // si abscisse+1 == deck->size -> on le met à black
+            if ( abscisse+1 == deck->size )
+            {
+                vertice_target = deck->black_2;
+            }
+            else
+            {
+                vertice_target = vertice_get_coordinates(deck, abscisse+1, ordonnee);
+            }
+            new_edge = edge_create(vertice_current, vertice_target);
+            deck->set_edges[counter_edges++] = new_edge;
+            
+            // b5 tout le temps
+                // sauf sur la dernière ligne mais pas la première de la dernière ligne
+                // sauf abscisse != 0 && ordonnee == deck->size-1
+            if ( !(abscisse != 0 && ordonnee == deck->size-1) )
+            {
+                vertice_target = vertice_get_coordinates(deck, abscisse+1, ordonnee-1);
+                new_edge = edge_create(vertice_current, vertice_target);
+                deck->set_edges[counter_edges++] = new_edge;
+            }
+            // b6 jamais
+        }
+    }
+    
+    assert( (counter_edges) == number_edges );
+    
     return deck;
 }
 void deck_print(Deck deck)
