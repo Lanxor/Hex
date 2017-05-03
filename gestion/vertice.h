8535fed4ef96eb4f0ce535181c059b7810dacf44
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-03 20:47:17 +0200

    Ajout du prototype de vérification de sommet si c'est une bordure

--- gestion/vertice.h
+++ gestion/vertice.h
@@ -14,6 +14,8 @@ typedef struct s_vertice *Vertice;
 Vertice vertice_create(char color, unsigned int abscisse,
                                    unsigned int ordonnee);
 
+int vertice_is_border(Vertice vertice, Deck deck);
+
 void vertice_modify_color(Vertice vertice, char color);
 
 unsigned int vertice_get_abscisse(Vertice vertice);
