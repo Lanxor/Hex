f5eb72540d367b7fb7124b606ac714146fed6802
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-03 15:56:20 +0200

    Maj : modification de la couleur d'un sommet

--- gestion/vertice.c
+++ gestion/vertice.c
@@ -28,7 +28,8 @@ void vertice_modify_color(Vertice vertice, char color)
 
 void vertice_print_coordinates(Vertice vertice)
 {
-    printf("%d,%d", vertice->abscisse, vertice->ordonnee);
+    // printf - %u unsigned int
+    printf("%u,%u", vertice->abscisse, vertice->ordonnee);
 }
 
 void vertice_print_color(Vertice vertice)
