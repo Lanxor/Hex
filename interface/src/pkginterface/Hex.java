f1f9fb56be87ad1073cc783fc2c80092275b9cde
Eléana <Elana@192.168.1.16>
2017-05-04 16:07:59 +0200

    choix joueurs

--- interface/src/pkginterface/Hex.java
+++ interface/src/pkginterface/Hex.java
@@ -6,27 +6,81 @@
  */
 package pkginterface;
 
+import java.util.Scanner;
+
 public class Hex {
     
-    private static Menu menu;
-    private static Deck tablier;
-    private int nbJoueurs = 2;
-    private static Player[] Joueurs;
-    private static Player Joueur1;
-    private static Player Joueur2;
+    private Menu menu;
+    private Deck tablier;
+    private Player[] Players;
+    private Player Player1;
+    private Player Player2;
     
-    public static void init()
+    private boolean isInteger(String coord) {
+       boolean isValid = true;
+       try
+       { 
+           Integer.parseInt(coord); 
+       }
+       catch(NumberFormatException nfe)
+       { 
+           isValid = false;
+       }
+       return isValid;
+    }
+    
+    public void init(int numJoueur)
     {
-        if (Joueurs.length != 0)
+        if (Players.length != 0)
         {
             System.out.println("Choisir des joueurs : ");
-            for (int cpt = 0; cpt < Joueurs.length; ++cpt){
-                System.out.println();
+            System.out.println("Joueur " + Integer.toString(numJoueur));
+            System.out.println("1 : Nouveau Joueur");
+            for (int cpt = 0; cpt < Players.length; ++cpt){
+                System.out.println(Integer.toString(cpt+2) 
+                        + " : " + Players[cpt].getPseudo());
             }
+            Scanner keyboard = new Scanner(System.in);
+            String choice = keyboard.next();
+            while (!isInteger(choice) 
+                || Integer.parseInt(choice) < 0 
+                || Integer.parseInt(choice) > 5){
+            System.out.print("erreur ");
+            choice = keyboard.next();
+            }
+            if (Integer.parseInt(choice) == 1)
+            {
+                if (numJoueur == 1)
+                {
+                    Player1 = new Player(tablier);
+                    Player1.init();
+                }
+                else
+                {
+                    Player2 = new Player(tablier);
+                    Player2.init();
+                }
+            }
+            else
+            {
+                if (numJoueur == 1)
+                    Player1 = Players[Integer.parseInt(choice)-2];
+                else
+                    Player2 = Players[Integer.parseInt(choice)-2];
+            }
+        }
+        else
+        {
+            System.out.println("Joueur 1");
+            Player1 = new Player(tablier);
+            Player1.init();
+            System.out.println("Joueur 2");
+            Player2 = new Player(tablier);
+            Player2.init();
         }
     }
     
-    public static void main(String[] args)
+    public void play()
     {      
         menu = new Menu();
         int resp = menu.Home();
@@ -34,18 +88,14 @@ public class Hex {
             
             tablier = new Deck();
             tablier.init();
-            System.out.println("Joueur 1");
-            Joueur1 = new Player(tablier);
-            Joueur1.init();
-            System.out.println("Joueur 2");
-            Joueur2 = new Player(tablier);
-            Joueur2.init();
+            init(1);
+            init(2);
             tablier.displayBoard();
         }
         
-        Joueur1.placeStone();
+        Player1.placeStone();
         tablier.displayBoard();
-        Joueur2.placeStone();
+        Player2.placeStone();
         tablier.displayBoard();
     }
     
