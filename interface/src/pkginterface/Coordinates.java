d3b030c7bb5872b542e6c79af5fb6fed037614cc
Eléana <Elana@192.168.1.16>
2017-05-04 17:11:19 +0200

    modif classe coord

--- interface/src/pkginterface/Coordinates.java
+++ interface/src/pkginterface/Coordinates.java
@@ -10,54 +10,12 @@ import java.util.Scanner;
 
 public class Coordinates {
     
-    private Deck deck;
     private int abscisse;
     private int ordonnee;
     
-    public Coordinates(Deck deck){
-        this.deck = deck;
-        this.abscisse = 0;
-        this.ordonnee = 0;
-    }
-    
-    private boolean isInteger(String coord) {
-       boolean isValid = true;
-       try
-       { 
-           Integer.parseInt(coord); 
-       }
-       catch(NumberFormatException nfe)
-       { 
-           isValid = false;
-       }
-       return isValid;
-   }
-    
-    public void setCoord()
-    {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("abs : ");
-        String input = keyboard.next();
-        while (!isInteger(input) 
-                || Integer.parseInt(input) < 0 
-                || Integer.parseInt(input) >= this.deck.getSize()){
-            System.out.print("Vous devez entrer un entier entre 0 et " 
-                    + Integer.toString(this.deck.getSize()-1)
-                    + ", veuillez recommencer.\n abs : ");
-            input = keyboard.next();
-        }
-        this.abscisse = Integer.parseInt(input);
-        System.out.print("ord : ");
-        input = keyboard.next();
-        while (!isInteger(input) 
-                || Integer.parseInt(input) < 0 
-                || Integer.parseInt(input) >= this.deck.getSize()){
-            System.out.print("Vous devez entrer un entier entre 0 et " 
-                    + Integer.toString(this.deck.getSize()-1)
-                    + ", veuillez recommencer.\n abs : ");
-            input = keyboard.next();
-        }
-        this.ordonnee = Integer.parseInt(input);
+    public Coordinates(int abs, int ord){
+        this.abscisse = abs;
+        this.ordonnee = ord;
     }
     
     public int getAbscisse()
