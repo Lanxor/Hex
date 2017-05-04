cbab0d0f537619008d3977deb7e5a3fa6d3bbbc1
Eléana <Elana@192.168.1.16>
2017-05-04 14:19:44 +0200

    verif choix menu = entier < nb possibilités

--- interface/src/pkginterface/Menu.java
+++ interface/src/pkginterface/Menu.java
@@ -10,22 +10,50 @@ import java.util.Scanner;
 
 public class Menu {
     
-    public void displayHome()
+        private boolean isInteger(String coord) {
+       boolean isValid = true;
+       try
+       { 
+           Integer.parseInt(coord); 
+       }
+       catch(NumberFormatException nfe)
+       { 
+           isValid = false;
+       }
+       return isValid;
+   }
+    
+    public int Home()
     {
         System.out.println("1 : Créer une nouvelle partie\n"
                 + "2 : Charger une partie\n"
                 + "3 : Quitter"
                 + "4 : Scores");
+        Scanner keyboard = new Scanner(System.in);
+        String choice = keyboard.next();
+        while (!isInteger(choice) 
+                || Integer.parseInt(choice) < 0 
+                || Integer.parseInt(choice) > 4){
+            System.out.print("erreur ");
+            choice = keyboard.next();
+        }
+        return Integer.parseInt(choice);
     }
     
-    public int choiceHome()
+    public int Choice()
     {
+        System.out.println("1 : Jouer\n"
+                + "2 : Sauvegarder\n"
+                + "3 : Sauvegarder et Quitter"
+                + "4 : Quitter sans sauvegarder");
         Scanner keyboard = new Scanner(System.in);
-        int choice = Integer.parseInt(keyboard.next());
-        switch (choice) {
-            case 1  : return 1;
-            case 3  : return 3;
-            default : return 0;
+        String choice = keyboard.next();
+        while (!isInteger(choice) 
+                || Integer.parseInt(choice) < 0 
+                || Integer.parseInt(choice) > 4){
+            System.out.print("erreur ");
+            choice = keyboard.next();
         }
+        return Integer.parseInt(choice);
     }
 }
