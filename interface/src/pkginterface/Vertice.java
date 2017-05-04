ae15209043a3073781ebeaa6e2ac53b83e946618
Eléana <Elana@192.168.1.16>
2017-05-04 17:12:26 +0200

    classe vertice

---
+++ interface/src/pkginterface/Vertice.java
@@ -0,0 +1,23 @@
+/**
+ * @file Cell.java
+ * @author PACE Eleana
+ * @date 26 avril 2017
+ * @brief Classe de gestion de la cellule
+ */
+
+package pkginterface;
+
+public class Vertice {
+    private String color;
+    
+    public Vertice()
+    {
+        this.color = "transparant";
+    }
+    
+    public String getColor()
+    {
+        return this.color;
+    }
+    
+}
