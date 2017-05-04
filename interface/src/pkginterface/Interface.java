b94ad46ae1c7b6d3845f39071da4f261538947e1
Eléana <Elana@192.168.1.16>
2017-05-04 16:08:21 +0200

    nouveau main

---
+++ interface/src/pkginterface/Interface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+
+/**
+ *
+ * @author Eléana
+ */
+public class Interface {
+    
+    public static void main(String[] args)
+    {
+        Hex hex = new Hex();
+        hex.play();
+    }
+    
+}
