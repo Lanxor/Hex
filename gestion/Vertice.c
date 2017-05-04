722bded2e416b0b1a6f0b22e8392bce368fbdda5
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-04 18:38:29 +0200

    Update projet NetBeans Doublons

--- gestion/Vertice.c
+++
@@ -1,7 +0,0 @@
-
-typedef struct s_vertic
-{
-    char color;
-    unsigned int abscisse;
-    unsigned int ordonnee;
-} t_vertice;
--- gestion/historical.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __HISTORICAL_H__
-#define __HISTORICAL_H__
-
-typedef struct t_historical *Historical;
-
-#endif
-
