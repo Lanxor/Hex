c0cca1d754d52d2390b96caf74e1769d2980d2d6
Eléana <Elana@192.168.1.16>
2017-05-04 18:23:17 +0200

    maj menu en fonction du tour

--- interface/src/pkginterface/Menu.java
+++ interface/src/pkginterface/Menu.java
@@ -14,17 +14,30 @@ public class Menu {
     {
         System.out.println("1 : Créer une nouvelle partie\n"
                 + "2 : Charger une partie\n"
-                + "3 : Quitter\n"
-                + "4 : Statistiques");
+                + "3 : Statistiques\n"
+                + "4 : Quitter");
     }
     
-    public void Choice()
+    public void Choice(int round)
     {
-        System.out.println("1 : Jouer\n"
-                + "2 : Revenir au coup précédent\n"
-                + "3 : Sauvegarder\n"
-                + "4 : Sauvegarder et Quitter\n"
-                + "5 : Quitter sans sauvegarder");
+        if (round == 1)
+            System.out.println("1 : Jouer\n"
+                + "2 : Sauvegarder\n"
+                + "3 : Sauvegarder et Quitter\n"
+                + "4 : Quitter sans sauvegarder");
+        else
+            if (round == 2)
+                System.out.println("1 : Jouer\n"
+                    + "2 : Echanger les couleurs\n"
+                    + "3 : Sauvegarder\n"
+                    + "4 : Sauvegarder et Quitter\n"
+                    + "5 : Quitter sans sauvegarder");
+            else
+                System.out.println("1 : Jouer\n"
+                        + "2 : Revenir au coup précédent\n"
+                        + "3 : Sauvegarder\n"
+                        + "4 : Sauvegarder et Quitter\n"
+                        + "5 : Quitter sans sauvegarder");
     }
     
     public void Statistics()
