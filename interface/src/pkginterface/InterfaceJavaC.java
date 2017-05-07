84fbff2b0e92509bfb2767739587059fca9148f4
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-07 19:22:33 +0200

    Ajout de la classe InterfaceJavaC

---
+++ interface/src/pkginterface/InterfaceJavaC.java
@@ -0,0 +1,12 @@
+/**
+ * @file InterfaceJavaC.java
+ * @author PACE Eleana
+ * @date 26 avril 2017
+ * @brief Interface des fonctions du c 
+ */
+
+package pkginterface;
+
+public class InterfaceJavaC {
+    
+}
