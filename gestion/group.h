47ab2f9475ac61151cfa0c958b475f76e182d88d
Xiodi <gwendal.patat@gmail.com>
2017-05-07 13:49:06 +0200

    Ajout des prototype des fonction create et insert avec doc doxygen

--- gestion/group.h
+++ gestion/group.h
@@ -1,3 +1,10 @@
+/**
+ * @file deck.h
+ * @autho PATA Gwendal - OLIVIER Thomas
+ * @date Mai 2017
+ * @brief Fichier de prototype des groupes du Jeu.
+ */
+
 #ifndef __GROUP_H__
 #define __GROUP_H__
 
@@ -7,7 +14,25 @@
 
 #include "vertice.h"
 
+/**
+ * @brief Structure des groupes de jeu.
+ * @typedef Structure des groupes de jeu
+ */
 typedef struct s_group *Group;
+
+/**
+ * @brief Crée un nouveau groupe de jeu
+ * @return Retourne un groupe.
+ */
+Group group_create();
+
+/**
+ * @brief Ajoute un vertice dans un groupe
+ * @param initialGroup : groupe où l'on insert le vertice
+ * @param verticeToInsert : vertice à insérer dans le groupe
+ * @return Retourne le groupe modifié avec la nouvelle vertice
+ */
+Group group_insert(Group initialGroup, Vertice verticeToInsert);
 
 #endif /* GROUP_H */
 
