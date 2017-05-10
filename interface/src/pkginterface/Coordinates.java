a0c5d5d65dab43e9a38992ac7cc4ba4acd37115d
Lanxor <Thomas.olivier3@yahoo.fr>
2017-05-10 15:57:50 +0200

    Maj : convertir en toString

--- interface/src/pkginterface/Coordinates.java
+++ interface/src/pkginterface/Coordinates.java
@@ -19,11 +19,6 @@ public class Coordinates {
         this.ordonnee = ord;
     }
     
-    public String getCoordonnee()
-    {
-        return this.getAbscisse() + " " + this.getOrdonnee();
-    }
-    
     public int getAbscisse()
     {
         return this.abscisse;
@@ -34,4 +29,8 @@ public class Coordinates {
         return this.ordonnee;
     }
     
+    public String toString()
+    {
+        return this.getAbscisse() + " " + this.getOrdonnee();
+    }
 }
