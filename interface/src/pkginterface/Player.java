00cdaed1fc3be84ff6693dcff33ef9e28a0b945d
Eléana <Elana@192.168.1.16>
2017-05-10 16:30:26 +0200

    string -> char

--- interface/src/pkginterface/Player.java
+++ interface/src/pkginterface/Player.java
@@ -10,23 +10,21 @@ package pkginterface;
 public class Player {
     
     private String pseudo;
-    private String color;
+    private char color;
     private String mail;
-    private int yearOfBirth;
     
-    public Player(String pseudo, String color, String mail, int yearOfBirth)
+    public Player(String pseudo, char color, String mail)
     {
         this.pseudo = pseudo;
         this.color = color;
         this.mail = mail;
-        this.yearOfBirth = yearOfBirth;
     }
     public String getPseudo()
     {
         return this.pseudo;
     }
     
-    public String getColor()
+    public char getColor()
     {
         return this.color;
     }
