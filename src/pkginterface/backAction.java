a8cd22319e9a5f0f7755fd8d8430d270988587a3
NanaBarbare <eleana.pace@univ-tlse3.fr>
2017-05-22 18:10:06 +0200

    echange joueurs après retour en arriere

--- src/pkginterface/backAction.java
+++ src/pkginterface/backAction.java
@@ -25,6 +25,9 @@ public class backAction extends AbstractAction{
     public void actionPerformed(ActionEvent e)
     {
         InterfaceSwing.getGame().goBackN(1);
+        Player temp = InterfaceSwing.getGame().getPlayer2();
+        InterfaceSwing.getGame().setPlayer2(InterfaceSwing.getGame().getPlayerCurrent());
+        InterfaceSwing.getGame().setPlayerCurrent(temp);
         this.fenetre.panel = new Deck(InterfaceSwing.getGame().getDeck().getSizeDeck());
         this.fenetre.panel.add(this.fenetre.buttons.menu("jeu"));
         this.fenetre.setContentPane(this.fenetre.panel);
